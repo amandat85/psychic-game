177f3eec267c40d93f946d972f894c24e89913f3
amandat <amandatome@me.com>
2019-01-30 06:01:19 -0500

    JavaScript
    Working on and returning variables.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,54 +2,61 @@
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var guessedLetters = []
-var losses = 0
-var wins = 0
-var guessesLeft = 10
-var random = []
+//Test Array
+console.log(letters);
+
+var guessedLetter = [];
+console.log(guessedLetter)
+var losses = 0;
+var wins = 0;
+var guessesLeft = 9;
 
 //Set up random letter
-window.onload = function() {
-    var computerRandom = letters(Math.floor(Math.random() * letters.length));
-    computerRandom.push(random);
-}
-console.log(computerRandom);
-
+var random = letters[Math.floor(Math.random() * letters.length)];
+//Test Random letter
+console.log(random);
 
 // Game Play
-document.onkeyup = function keyPress(event) {
-    var playerInput = event.key;
-	guessedLetters.push(playerInput);
-
+document.onkeyup = function(event) {
+    key = event.key.toLowerCase();
+    console.log(key);
+    //not in scope?
+    var guessedLetter = [];
+    guessedLetter.push(key);
+    console.log(guessedLetter);
+    //return guessedLetter;
 }
+
+
 // Conditions
-//If the player (playInput) guesses the randomly generated letter (random) and guesses (guessesLeft) are above 0
-if ((playerInput === random) && (guessesLeft > 0)) {
+//If the player guesses the randomly generated letter (random) and guesses (guessesLeft) are above 0
+
+
+if (("guessedLetter" === random) && (guessesLeft > 0)) {
     //var win goes up by 1
     wins++;
+    console.log(wins);
     //guessedLetters resets
     guessedLetter.length = 0;
     //guessesLeft resets
-    guessedLetter.length = 0;
+    guessedLeft.length = 9;
     //New letter generated randomly from array
-    var computerRandom = letters(Math.floor(Math.random() * letters.length));
-    computerRandom.push(random);
+    var random = letters(Math.floor(Math.random() * letters.length));
 }
 
-else if ((playInput !== random) && (guessesLeft > 0)) {
+else if (("guessedLetter" !== random) && (guessesLeft > 0)) {
     //Then guessesLeft goes down by one
     guessesLeft=guessesLeft-1;
 }
 
 else {
-    //loses increase
+    //losses increase
     losses ++;
     //guessedLetter is reset
     guessedLetter.length = 0;
     //guessesLeft resests
     guessesLeft=guessesLeft + 10;
     //game is reset and letter chosen from array
-    var computerRandom = letters(Math.floor(Math.random() * letters.length));
-    computerRandom.push(random);
-
+    var random = letters(Math.floor(Math.random() * letters.length));
+    random.push(random);
 }
