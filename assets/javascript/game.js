08efcef3b195e388e7085c821a7a63dafe2c4234
amandat <amandatome@me.com>
2019-01-30 22:47:17 -0500

    JavaScript
    Discovered one of the Javascript problems and fixed it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,77 @@
-// Variables
+//Variables
 
 //Letter Array
-
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-//Variables to hold stats
-var guessedLetter = [];
-var losses = 0;
+//Stats Variables
 var wins = 0;
+var losses = 0;
+var guessedLetter= [];
 var guessesLeft = 10;
 
-//Variables to reference HTML elements
-//document.getElementById("wins-text").innerHTML;
-//var lossesText = document.getElementById("losses-text").innerHTML;
-//var leftText = document.getElementById("left-text").innerHTML;
+//Reset/Game Start Function
+function gameStart () {
+    //Reset Variables and Display
+    wins = 0;
+    document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+    losses = 0;
+    document.getElementById("losses-text").innerHTML = "Losses: " + losses;
+    guessedLetter= [];
+    document.getElementById("left-text").innerHTML = "Guesses Left: " + guessesLeft;
+    guessesLeft = 10;
+    //Spacing
+    document.getElementById("guessed-text").innerHTML = "Your Guessed Letters: " + guessedLetter + " ";
+}
 
-//Display user stats
-document.getElementById("wins-text").innerHTML = "Wins: " + wins;
-document.getElementById("losses-text").innerHTML = "Losses: " + losses;
-document.getElementById("left-text").innerHTML = "Guesses Left: " + guessesLeft;
-//Space between logged guessed letterrs is not working
-document.getElementById("guessed-text").innerHTML = "Your Guessed Letters: " + guessedLetter + " ";
+gameStart();
 
- //Set up random letter
-var random = letters[Math.floor(Math.random() * letters.length)];
-console.log(random);
+//Generate Random Letter
+    var random = letters[Math.floor(Math.random() * letters.length)];
+    console.log(random);
 
 // Game Play
 document.onkeyup = function(event) {
+
     var key = event.key.toLowerCase();
-    //console.log(key);
+    console.log(key);
 
-     //User guessed letter gets pushed to array
-     guessedLetter.push(key);
-     console.log(guessedLetter);
-     
-//Conditions     
-     //Wrong Guess
-        if ((guessedLetter !== random) && (guessesLeft > 0)) {
-            //Guesses left descreases by one
-           guessesLeft = guessesLeft - 1;
-           document.getElementById("left-text").innerHTML = "Guesses Left: " + guessesLeft;
-           console.log(guessesLeft);
-            //Guessed letters get logged for view
-           document.getElementById("guessed-text").innerHTML = "Your Guessed Letters: " + guessedLetter;
-        }
-    //Right Guess still have guesses left (if else)
-       else if (guessedLetter === random) {
-            //Win increase by one
-            wins++;
-            document.getElementById("wins-text").innerHTML = "Wins: " + wins;
-            console.log(wins);
-            //Guesses left remains the same
-           guessesLeft = guessesLeft;
-           console.log(guessesLeft);
-            //Random array gets launched
-            //var random = letters[Math.floor(Math.random() * letters.length)];
-            //console.log(random);
-       }
-       else {
-           losses++; 
-           //reset
-       }
+//User guessed letter gets pushed to array
+    guessedLetter.push(key);
+    console.log(guessedLetter);
+    document.getElementById("guessed-text").innerHTML = "Your Guessed Letters: " + guessedLetter;
+
+    //Conditions     
+     //Right Guess
+     if ((key === random) && (guessesLeft > 0)) {
+        wins++;
+        document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+        console.log(wins);
+        //reset guessed letters array
+        //randomLetter();  
+    }    
+    else if ((key !== random) && (guessesLeft > 0)) {
+        guessesLeft = guessesLeft - 1;
+        document.getElementById("left-text").innerHTML = "Guesses Left: " + guessesLeft;
+        console.log(guessesLeft);
     }
-// Conditions
-//Wrong Guess (if) - greater than zero
-    //guesses left descreases by one
-    //Guessed letters get logged for view
-    
-//Right Guess still have guesses left (if else)
-    //Win increase by one
-    //Guesses left remains the same
-    //Random array gets launched
 
-//Game Over
-    //Losses increase by 1
-    //Guesses left gets reset
-    //Game reset - random array gets launched
+    else if (guessesLeft === 0) {
+        losses++;
+        randomLetter();
+        //gameStart();
+    }
+}
 
-//Text Display
-    //Wins
-    //Losses
-    //Guesses Left
-    //Letters Guessed
+//Reset Variables for game
+function resetVariables () {
+    wins = 0;
+    losses = 0;
+    guessedLetter= [];
+    guessesLeft = 10;
+}
+
+//Generate Random Letter function
+function randomLetter() {
+    var random = letters[Math.floor(Math.random() * letters.length)];
+    console.log(random);
+}
